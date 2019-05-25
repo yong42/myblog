030866b286e027fde1cc6d2dd22d8ce57c3999bf
yong42 <374813543@qq.com>
2019-05-25 08:51:22 +0800

    常用模板方法

--- myblog/urls.py
+++ myblog/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
+
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ueditor/', include('DjangoUeditor.urls')),
+    re_path('^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('', views.index)
 ]
